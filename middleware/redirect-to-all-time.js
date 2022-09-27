cd5a7572f5b4cd38ed39eef68d03b07e76ce52d2
zdoryk <nice.zdorik@gmail.com>
2022-09-27 12:12:28 +0200

    New redirect middleware has been added.

--- middleware/redirect-to-all-time.js
+++ middleware/redirect-to-all-time.js
@@ -1,7 +1,7 @@
 export default function ({ store, redirect }) {
   // If the user is not authenticated
   // if (!store.state.authenticated) {
-  if (screen.width <= 420) return redirect(301, '/AllReceipts/AllTimeMobile');
-  else return redirect(301, '/AllReceipts/AllTime');
+  if (screen.width <= 420) return redirect(301, '/AllReceipts/Mobile');
+  else return redirect(301, '/AllReceipts/Desktop');
   // }
 }
