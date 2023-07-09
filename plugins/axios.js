7b83588ff42ceec6f4d1854757fec57d0adf418e
Danylo Zdoryk <nice.zdorik@gmail.com>
2023-07-09 20:11:04 +0200

    Update axios.js

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,13 +1,16 @@
-export default function ({ store, $axios, redirect }) {
+export default function ({ store, $axios }) {
   $axios.onRequest((config) => {
-    // config.headers['Access-Control-Allow-Origin'] = '*'
     // check if the user is authenticated
     if (store.state.state.access_token) {
       // set the Authorization header using the access token
-      console.log("true");
-      config.headers.Authorization = "Bearer " + store.state.state.access_token;
+      config.headers.Authorization = 'Bearer ' + store.state.state.access_token;
     }
-    console.log("false");
+
+    // Add your CORS headers here
+    config.headers.common['Access-Control-Allow-Origin'] = '*';
+    config.headers.common['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS, PUT, PATCH, DELETE';
+    config.headers.common['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept';
+    config.headers.common['Access-Control-Allow-Credentials'] = true;
 
     return config;
   });
