2fa1a138fb026af63ceb6c88ce38e9c1edb45b14
Danylo Zdoryk <nice.zdorik@gmail.com>
2023-07-09 20:24:43 +0200

    Update axios.js

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,14 +1,13 @@
-export default function ({ store, $axios }) {
+export default function ({ store, $axios, redirect }) {
   $axios.onRequest((config) => {
+    // config.headers['Access-Control-Allow-Origin'] = '*'
     // check if the user is authenticated
     if (store.state.state.access_token) {
       // set the Authorization header using the access token
-      config.headers.Authorization = 'Bearer ' + store.state.state.access_token;
+      console.log("true");
+      config.headers.Authorization = "Bearer " + store.state.state.access_token;
     }
-
-    // Disable preflight requests
-    config.headers.common['Access-Control-Allow-Origin'] = '*';
-    config.headers.common['Access-Control-Allow-Methods'] = '*';
+    console.log("false");
 
     return config;
   });
