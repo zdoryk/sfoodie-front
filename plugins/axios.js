46ef1e3a27a9e0de6d65a7d2f054ec6445f32248
Danylo Zdoryk <nice.zdorik@gmail.com>
2023-07-09 20:13:45 +0200

    Update axios.js

--- plugins/axios.js
+++ plugins/axios.js
@@ -6,11 +6,9 @@ export default function ({ store, $axios }) {
       config.headers.Authorization = 'Bearer ' + store.state.state.access_token;
     }
 
-    // Add your CORS headers here
+    // Disable preflight requests
     config.headers.common['Access-Control-Allow-Origin'] = '*';
-    config.headers.common['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS, PUT, PATCH, DELETE';
-    config.headers.common['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept';
-    config.headers.common['Access-Control-Allow-Credentials'] = true;
+    config.headers.common['Access-Control-Allow-Methods'] = '*';
 
     return config;
   });
